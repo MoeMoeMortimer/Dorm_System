2b4838b114f6665cbe7cefdec13dc413b103e7f3
MoeMoeMortimer <nyankochan611@gmail.com>
2017-12-31 12:06:48 +0800

    这个是新的数据库文件

--- documents/Drom_view.sql
+++ documents/Drom_view.sql
@@ -5,14 +5,14 @@ drop view SmyAsset;
 drop view tManageStu;
 drop view tStuLeave;
 drop view tFixInfo;
-drop view tStuCurfew;
+drop view tStuMail;
 
 
--- 学生晚归信息
+-- 学生晚归信息（教师管理学生晚归也用这个）
 create view StuCurfew as
 	select Student.Sno,Sname,Dno,NightTime,NightReason,Tno 
-	from Student,Curfew,SD
-	where Student.Sno=Curfew.Sno and Student.Sno=SD.Sno;
+	from Student,Curfew
+	where Student.Sno=Curfew.Sno;
 	
 -- 学生报修信息
 create view StuFix as 
@@ -23,8 +23,8 @@ create view StuFix as
 -- 学生查看宿舍信息
 create view SmyDorm as 
 	select Bblock,Building.Bno,Dormitory.Dno,Btime,Dtel,Scin
-	from Dormitory,SD,Building
-	where SD.Dno=Dormitory.Dno and Building.Bno=Dormitory.Bno;
+	from Dormitory,Student,Building
+	where Student.Dno=Dormitory.Dno and Building.Bno=Dormitory.Bno;
 
 -- 学生查看宿舍财产信息(教师查看学生宿舍财产信息也用这个！)
 create view SmyAsset as 
@@ -32,17 +32,12 @@ create view SmyAsset as
 	from DA,Asset
 	where DA.Ano=Asset.Ano;
 
--- 教师管理学生信息
-create view tManageStu as
-	select Student.Sno,Sname,Ssex,Sgrade,Sdept,Dno,Scin
-	from Student,SD
-	where Student.Sno=SD.Sno;
 
 -- 教师管理学生离校信息
 create view tStuLeave as
 	select Student.Sno,Sname,Dno,Sltime,Sreturn
-	from Student,SD,LeaveSchool
-	where Student.Sno=SD.Sno and Student.Sno=LeaveSchool.Sno;
+	from Student,LeaveSchool
+	where Student.Sno=LeaveSchool.Sno;
 
 -- 教师管理报修信息
 create view tFixInfo as
@@ -50,9 +45,9 @@ create view tFixInfo as
 	from Asset,Repairs
 	where Asset.Ano=Repairs.Ano;
 
--- 教师管理学生晚归信息
-create view tStuCurfew as
-	select Student.Sno,Sname,Dno,NightTime,NightReason,Tno
-	from Student,Curfew,SD
-	where Student.Sno=SD.Sno and Student.Sno=Curfew.Sno;
 
+-- 教师管理学生快递信息
+create view tStuMail as
+	select Mno,Student.Sno,Sname,Dno,Marrive,Maccept
+	from Student,Mail
+	where Student.Sno=Mail.Sno;
